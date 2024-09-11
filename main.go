8295888b47e527a1c9c6ca656c52b7e248070eef
Naveen2070 <naveenrameshcud@gmail.com>
2024-09-11 21:45:29 +0530

    #done calculation need to start 4 fetch news on stock #3:21:00

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"math"
 	"os"
 	"slices"
 	"strconv"
@@ -53,11 +54,68 @@ func load(filename string) ([]Stock, error) {
 
 	return stocks, nil
 }
+
+var accountBalance = 100.00
+var lossTolerance = 0.02
+var maxLossPerTrade = accountBalance * lossTolerance
+var profitPercentage = 0.08
+
+type Position struct {
+	EntryPrice       float64
+	Shares           int
+	TakeProfilePrice float64
+	StopLossPrice    float64
+	Profit           float64
+}
+
+func caculate(gapPercent, openingPrice float64) Position {
+	closingPrice := openingPrice / (1 + gapPercent)
+	gapValue := closingPrice - openingPrice
+	profitFromGap := gapValue * profitPercentage
+
+	stopLoss := openingPrice - profitFromGap
+	takeProfit := closingPrice + profitFromGap
+
+	shares := int(maxLossPerTrade / math.Abs(stopLoss-openingPrice))
+
+	profit := math.Abs(openingPrice-takeProfit) * float64(shares)
+	profit = math.Round(profit*100) / 100
+
+	return Position{
+		EntryPrice:       math.Round(openingPrice*100) / 100,
+		Shares:           shares,
+		TakeProfilePrice: math.Round(takeProfit*100) / 100,
+		StopLossPrice:    math.Round(stopLoss*100) / 100,
+		Profit:           math.Round(profit*100) / 100,
+	}
+}
+
+type Selection struct {
+	Ticker string
+	Position
+}
+
 func main() {
 	stocks, err := load("./opg.csv")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(stocks)
+
+	slices.DeleteFunc(stocks, func(stock Stock) bool {
+		return math.Abs(stock.gap) < 0.1
+	})
+
+	var selections []Selection
+
+	for _, stock := range stocks {
+		position := caculate(stock.gap, stock.openingPrice)
+
+		selected := Selection{
+			Ticker:   stock.ticker,
+			Position: position,
+		}
+
+		selections = append(selections, selected)
+	}
 }
